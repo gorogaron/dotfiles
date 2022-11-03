2ed4cc4638d39fc0f66c32f39183d30b2cf59238
Görög Áron <gorogaron95@gmail.com>
2022-11-03 21:46:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
 
-cp -i .bashrc ~/
-cp -i .bash_profile ~/
+force_copy="false"
+
+while test $# -gt 0; do
+  case "$1" in
+    --force)
+      echo "Overwriting dotfiles without asking for confirmation..."
+      force_copy=true
+      shift
+      ;;
+    *)
+      shift
+      ;;
+  esac
+done
+
+if $force_copy ; then
+    yes | cp .bashrc ~/
+    yes | cp .bash_profile ~/
+else
+    cp -i .bashrc ~/
+    cp -i .bash_profile ~/
+fi
